5c51d3eb651b8370bca9b5bbfb2e6d6cf4648504
Entropy-rgb <som3shkamad@gmail.com>
2026-05-04 02:36:23 +0530

    fix : fix perf.py tests to display accurate results and stop re-counting

--- perf.py
+++ perf.py
@@ -3,25 +3,29 @@ from move_generator import move_generator
 from game import move, promote_pawn
 from config import pieces_enum
 import numpy as np
-
+from pprint import pprint
+DEPTH = 4
 count = 0
 capture = 0
 en_pasant = 0
 castle = 0
 promotion = 0
 
+CAPTURE_FLAGS = {1, 3, 12, 14, 16, 18}
+PROMOTION_FLAGS = {11, 12, 13, 14, 15, 16, 17, 18}
+
 test_brd = [
-    [pieces_enum.BLACK_ROOK, 0, 0, 0, 6, 0, 0, 4],
-    [1, 0, 1, 1, 5, 1, 3, 0],
-    [3, 2, 0, 0, 1, 2, 1, 0],
-    [0, 0, 0, -1, -2, 0, 0, 0],
-    [0, 1, 0, 0, -1, 0, 0, 0],
-    [0, 0, -2, 0, 0, -5, 0, 1],
-    [-1, -1, -1, -3, -3, -1, -1, -1],
-    [-4, 0, 0, 0, -6, 0, 0, -4],
+    [0]*8,
+    [0,0,1,0,0,0,0,0],
+    [0,0,0,1,0,0,0,0],
+    [-6,-1,0,0,0,0,0,4],
+    [0,-4,0,0,0,1,0,6],
+    [0]*8,
+    [0,0,0,0,-1,0,-1,0],
+    [0]*8,
 ]
 
-test_board = Board(np.array(test_brd), 0, (8, 8), [1, 1, 1, 1], 0, [0, 0, 0, 0])
+test_board = Board(np.array(test_brd), 0, (8, 8), [0,0,0,0], 0, [1,1,1,1])
 
 
 def test_move_generator(board=board, depth=1):
@@ -31,7 +35,7 @@ def test_move_generator(board=board, depth=1):
         return
     moves = move_generator(board)
     for pos in moves:
-
+        
         cpy = Board(
             board.board.copy(),
             board.turn,
@@ -42,19 +46,18 @@ def test_move_generator(board=board, depth=1):
         )
         done = move(pos.initial, pos.final, None, cpy)
         if done is None:
-            if pos.flag == 4:
-                castle+=1
             continue
-        if pos.flag == 1:
-            capture+=1
-        if pos.flag == 4:
-            castle+=1
-        if pos.flag == 3:
-            en_pasant+=1
-        if pos.flag in [11,12,13,14,15,16,17,18]:
-            promotion+=1
+        if depth == 1:
+            if pos.flag in CAPTURE_FLAGS:
+                capture += 1
+            if pos.flag == 4:
+                castle += 1
+            if pos.flag == 3:
+                en_pasant += 1
+            if pos.flag in PROMOTION_FLAGS:
+                promotion += 1
         test_move_generator(cpy, depth - 1)
 
 
-test_move_generator(test_board, 1)
+test_move_generator(board, DEPTH)
 print(f"{count=} {en_pasant=} {castle=} {promotion=} {capture=}")
